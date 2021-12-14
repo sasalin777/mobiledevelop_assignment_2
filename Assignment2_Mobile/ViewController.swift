78f525445e562dd3ff74ab38ed153a58486a3301
sasalin777 <candysasame@gmail.com>
2021-12-14 11:08:09 -0800

    upload assignment2

---
+++ Assignment2_Mobile/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Assignment2_Mobile
+//
+//  Created by Li Tzu Lin on 2021-12-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        //view.backgroundColor = UIColor(patternImage: UIImage(named: "bcgaribaldi_ake")!)
+
+    }
+   
+
+
+    
+}
+    
